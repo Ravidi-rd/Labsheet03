71be4aa7fa1d37690a56f6dcf6dfdc21b260a470
Ravidi-rd <rathnasriravidi@gmail.com>
2020-03-27 21:43:37 +0530

    labsheet03

---
+++ app/src/main/java/com/example/labsheet03/DisplayMessageActivity.java
@@ -0,0 +1,27 @@
+package com.example.labsheet03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+import static com.example.labsheet03.MainActivity.*;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView textView = (TextView) findViewById(R.id.textView) ;
+        textView.setText(message);
+
+
+    }
+
+}
